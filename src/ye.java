4cd4123d5af5d6365f36a829199913677d70362b
Rian Delou <riandelou63@hotmail.com>
2023-02-23 20:16:58 -0300

    testes

---
+++ src/ye.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ye {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num1;
+        int num2;
+        int sum;
+
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+
+        sum = num1 + num2;
+
+        System.out.print("A soma desses dois números é: "+ sum);
+
+    }
+}
